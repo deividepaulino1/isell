c849280b1cc0243cba09945f4a59b0f38eefe07b
Deivide Paulino <79219267+deividepaulino1@users.noreply.github.com>
2022-07-06 16:50:16 -0300

    criados os temas dark e light e tela de ajustes

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,38 @@
 import 'package:flutter/material.dart';
-import 'package:isell/pages/ajustes-page.dart';
+import 'package:isell/components/theme/themeChanger.dart';
+import 'package:isell/pages/ajustes/ajustes-page.dart';
 import 'package:isell/pages/caixa-page.dart';
 import 'package:isell/pages/clientes/editarClientes-page.dart';
 import 'package:isell/pages/home-page.dart';
 import 'package:isell/pages/login-page.dart';
 import 'package:isell/pages/produtos/editarProdutos-page.dart';
 import 'package:page_transition/page_transition.dart';
+import 'package:provider/provider.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
 
 import 'pages/vendas/visualizar-venda-page.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MultiProvider(
+      providers: [
+        ChangeNotifierProvider<ThemeChanger>(
+          create: (_) => ThemeChanger(),
+        )
+      ],
+      child: iSell(),
+    ));
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class iSell extends StatelessWidget {
+  const iSell({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   Widget build(BuildContext context) {
+    bool darkThemeEnabled = Provider.of<ThemeChanger>(context).isDark();
+
     return ResponsiveSizer(
       builder: (context, orientation, screenType) {
         return MaterialApp(
           debugShowCheckedModeBanner: false,
           title: 'iSell, seu pdv mobile',
-          theme: ThemeData(
-            primarySwatch: Colors.blue,
-          ),
           onGenerateRoute: (settings) {
             //rotas aqui, seguir este swich
             switch (settings.name) {
@@ -72,6 +78,9 @@ class MyApp extends StatelessWidget {
                 return null;
             }
           },
+          theme: ThemeChanger().light,
+          darkTheme: ThemeChanger().dark,
+          themeMode: darkThemeEnabled ? ThemeMode.dark : ThemeMode.light,
         );
       },
     );
