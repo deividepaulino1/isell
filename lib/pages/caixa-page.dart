deb1dadedbc235b0af845c067becd5ceab3b113a
Daniel Manoel <daniellfmanoel@gmail.com>
2022-07-06 13:25:16 -0300

    layout-caixa-pronto

--- lib/pages/caixa-page.dart
+++ lib/pages/caixa-page.dart
@@ -19,7 +19,35 @@ class _CaixaPageState extends State<CaixaPage> {
 
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Text('Caixa'),
+      child: GestureDetector(
+        onTap: () {
+          Navigator.pushNamed(context, '/');
+        },
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 60),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/logotipo/logotipo.png',
+                  width: 200,
+                  height: 200,
+                ),
+                Padding(padding: EdgeInsets.all(60)),
+                Text(
+                  'CAIXA LIVRE',
+                  style: TextStyle(fontSize: 45, fontWeight: FontWeight.bold, color: Color.fromRGBO(147, 22, 255, 1)),
+                ),
+                Text(
+                  'Toque para Vender',
+                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold, color: Color.fromRGBO(111, 108, 110, 0.5)),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
